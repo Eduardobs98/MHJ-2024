7d56d2d886b484ea5306f3a3a3c28e4e82704920
Eduardo <130837356+Eduardobs98@users.noreply.github.com>
2024-04-27 11:25:43 +0200

    no se si va

--- Formula33/Assets/Scripts/createNote.cs
+++ Formula33/Assets/Scripts/createNote.cs
@@ -11,13 +11,18 @@ public class createNote : MonoBehaviour
     public GameObject tecla;
     public List<Vector4> notas1;
     public List<Vector4> notas2;
+    public List<GameObject> teclasP1;
+    public List<GameObject> teclasP2;
     public float timeBetweenNotes;
     float timerBetweenNotes;
+    public miniGame minigame;
+    bool secondPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         timerBetweenNotes = 0;
+        secondPlayer = minigame.controlador.secondPlayer;
     }
 
     // Update is called once per frame
@@ -31,41 +36,79 @@ public class createNote : MonoBehaviour
             {
                 int cual;
                 cual = Random.Range(1, 3);
-                if (cual == 1)
+                if (!secondPlayer)
                 {
-                    mirarTeclas(notas1[Random.Range(0,notas1.Count)]);
+                    if (cual == 1)
+                    {
+                        mirarTeclas(notas1[Random.Range(0, notas1.Count)],secondPlayer);
+                    }
+                    else
+                    {
+                        mirarTeclas(notas2[Random.Range(0, notas2.Count)],secondPlayer);
+                    }
                 }
                 else
                 {
-                    mirarTeclas(notas2[Random.Range(0, notas2.Count)]);
+                    if (cual == 1)
+                    {
+                        mirarTeclas(notas1[Random.Range(0, notas1.Count)], secondPlayer);
+                    }
+                    else
+                    {
+                        mirarTeclas(notas2[Random.Range(0, notas2.Count)], secondPlayer);
+                    }
                 }
+                
             }
         }
     }
 
-    void cNote(Transform posicion)
+    void cNote(Transform posicion,GameObject tecla)
     {
         GameObject teclita = Instantiate(tecla, posicion);
         teclita.transform.parent = null;
     }
 
-    void mirarTeclas(Vector4 queTeclas)
+    void mirarTeclas(Vector4 queTeclas,bool player)
     {
-        if (queTeclas.x == 1)
+        if (!player)
         {
-            cNote(nota1);
+            if (queTeclas.x == 1)
+            {
+                cNote(nota1,teclasP1[0]);
+            }
+            if (queTeclas.y == 1)
+            {
+                cNote(nota2, teclasP1[1]);
+            }
+            if (queTeclas.z == 1)
+            {
+                cNote(nota3, teclasP1[2]);
+            }
+            if (queTeclas.w == 1)
+            {
+                cNote(nota4, teclasP1[3]);
+            }
         }
-        if (queTeclas.y == 1)
+        else
         {
-            cNote(nota2);
+            if (queTeclas.x == 1)
+            {
+                cNote(nota1, teclasP2[0]);
+            }
+            if (queTeclas.y == 1)
+            {
+                cNote(nota2, teclasP2[1]);
+            }
+            if (queTeclas.z == 1)
+            {
+                cNote(nota3, teclasP2[2]);
+            }
+            if (queTeclas.w == 1)
+            {
+                cNote(nota4, teclasP2[3]);
+            }
         }
-        if (queTeclas.z == 1)
-        {
-            cNote(nota3);
-        }
-        if (queTeclas.w == 1)
-        {
-            cNote(nota4);
-        }
+
     }
 }
